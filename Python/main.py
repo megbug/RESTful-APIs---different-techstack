4a4c170e6f917bd4cca5cdef4c224a151a8adaad
megbug <meghub@proton.me>
2023-10-31 19:29:16 +0100

    got POST method to work

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,16 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello" : "World"}
+
+@app.post("/user")
+async def user(req : Request):
+    req_body = await req.json()
+    return{
+        "data" : req_body,
+        "answer" : "blob"
+    }
